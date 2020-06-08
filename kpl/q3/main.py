c39e2afeeb05b8bbcb6e0a472c1e078c539c91e5
red-hat01 <carefreelakshay97@gmail.com>
2020-06-08 20:24:52 +0530

    q3

--- kpl/q3/main.py
+++ kpl/q3/main.py
@@ -1,18 +1,23 @@
 # all imports below
 
+import math
 """
 Any extra lines of code (if required)
 as helper for this function.
 """
 
+R = 5.974e24
+
+C = 2.998e8
+
+G = 6.674e−11
+R = 6357000
+
+# time dilation = sqrt(1- 2*G*M/r*c*c ) * T - Where T is the time taken for light to reach the satellite 
+
+def calcTime(dist):
+	return C * (R-dist)
+
 def findDelay(dist):
-	'''
-	Parameters
-	----------
-	dist : A `float`
-	
-	Returns
-	-------
-	A `float`
-	'''
-	return NotImplementedError
+	T = calcTime(dist)
+	dT = math.sqrt(1- (2*G*M)/(R*C*C)) * T
