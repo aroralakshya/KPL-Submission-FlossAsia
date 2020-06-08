b0fa260eb7387a77401fc3be59a6915dc83ad076
red-hat01 <carefreelakshay97@gmail.com>
2020-06-08 20:56:09 +0530

    q5

--- kpl/q5/main.py
+++ kpl/q5/main.py
@@ -1,50 +1,25 @@
 # all imports below
 
-"""
-Any extra lines of code (if required)
-as helper for this function.
-"""
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 class ScraperXRT:
-    '''
-    Description
-    -----------
-    A class to scrap XRT files from the telescope archive.
-    '''
+	def __init__(self, typeof_file, startime, endtime):
+		self.URL = 'http://solar.physics.montana.edu/HINODE/XRT/QL/syn_comp_fits/'
+		self.pageResponse = requests.get(self.URL)
+		self.bsParser = BeautifulSoup(self.pageResponse.content, 'html.parser')
+		self.trElements = self.bsParser.find_all('tr')
 
-    def __init__(self, typeof_file, startime, endtime):
-    '''
-    Parameters
-	----------
-	typeof_file: A `string`
-    startime: A `~datetime.datetime` instance
-    endtime: A `~datetime.datetime` instance
-	'''
+	def query(self):
+		print(self.trElements[3])
+		
 
-    def query(self):
-	'''
-	Returns
-	-------
-	A `list` of strings of URLs.
-	'''
-	return NotImplementedError
+	def get(self):
+		return NotImplementedError
 
-    def get(self):
-	'''
-	Returns
-	-------
-	A `list` of strings for files.
-	'''
-	return NotImplementedError
+	def view(self, filepath):
+		return NotImplementedError
 
-    def view(self, filepath):
-	'''
-    Parameters
-	----------
-    filepath: A `string` representing absolute path of file in system.
-
-	Returns
-	-------
-	An instance of `matplotlib.image.AxesImage`, returned using `plt.imshow(data)`.
-	'''
-	return NotImplementedError
+scraper = ScraperXRT('AI_mesh', datetime.now(), datetime.now())
+scraper.query()
